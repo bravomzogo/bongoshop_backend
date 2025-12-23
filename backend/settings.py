5cf81ae2c0959c294c513fd3b5a928cc03112dec
bravomzogo <bravomzogo@gmail.com>
2025-12-24 01:54:56 +0300

    sqlite

--- backend/settings.py
+++ backend/settings.py
@@ -153,16 +153,12 @@ TEMPLATES = [
 # ----------------------------------------------------
 # DATABASE (SQLite default)
 # ----------------------------------------------------
-import dj_database_url
-
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=not DEBUG,
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
-
 
 
 # Custom user model
