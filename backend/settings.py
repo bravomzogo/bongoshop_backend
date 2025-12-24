e724a7c11e684ab478a99e940c74a8b3f824f263
bravomzogo <bravomzogo@gmail.com>
2025-12-24 03:04:17 +0300

    render postgres

--- backend/settings.py
+++ backend/settings.py
@@ -153,12 +153,16 @@ TEMPLATES = [
 # ----------------------------------------------------
 # DATABASE (SQLite default)
 # ----------------------------------------------------
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=not DEBUG,
+    )
 }
+
 
 
 # Custom user model
